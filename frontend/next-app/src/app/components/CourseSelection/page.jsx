89dce5d2f6d71fffb1b6679c3b54ea0edc559851
MTRFYMD <mtrfymd@gmail.com>
2024-08-17 16:39:54 +0900

    アプリのスタート画面作成・ログイン/サインアップのデザイン修正

--- frontend/next-app/src/app/components/CourseSelection/page.jsx
+++ frontend/next-app/src/app/components/CourseSelection/page.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+'use client';
 
-const CourseSelection = () => {
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+export default function CourseSelection() {
+  const router = useRouter();
+
+  const handleConsideringDog = () => {
+    router.push('/components/ConsideringDogTop');
+  };
+
+  const handleCaringForDog = () => {
+    router.push('/components/login');
+  };
+
   return (
-    <div class="flex flex-col justify-center items-center gap-4">
-      <h1 class="text-2xl mt-12">コース選択</h1>
-
-
+    <div className="flex flex-col justify-center items-center gap-4">
+      <h1 className="text-2xl mt-12">コース選択</h1>
       <h2 class="mt-12">犬を飼う前の</h2>
-      <button class="btn btn-wide bg-yellow-400 rounded-full mt-2">検討中コース</button>
+      <button 
+        className="btn btn-wide bg-yellow-400 rounded-full mt-6"
+        onClick={handleConsideringDog}
+      >
+        検討中コース
+      </button>
       <h2 class="mt-12">犬を飼っている</h2>
-      <button class="btn btn-wide bg-yellow-400 rounded-full mt-2">おせわコース</button>
+      <button 
+        className="btn btn-wide bg-yellow-400 rounded-full mt-6"
+        onClick={handleCaringForDog}
+      >
+        おせわコース
+      </button>
     </div>
-  )
-} 
-
-export default CourseSelection
+  );
+}
